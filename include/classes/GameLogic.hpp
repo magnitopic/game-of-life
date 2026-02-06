03a978836d4ee87653467c4a96e0f14fe5afa907
magnitopic <magnitopic@gmail.com>
2026-02-06 20:12:44 +0100

    feat: added mouse & key hook

--- include/classes/GameLogic.hpp
+++ include/classes/GameLogic.hpp
@@ -9,6 +9,11 @@ class GameLogic
 private:
 	Screen *screen;
 
+	// Mouse event handling
+	static void mouseButtonCallback(mouse_key_t button, action_t action, modifier_key_t mods, void *param);
+	// Key event handling
+	static void keyPressCallback(mlx_key_data_t keydata, void *param);
+
 public:
 	GameLogic();
 	GameLogic(const GameLogic &copy);
--- src/classes/GameLogic.cpp
+++ src/classes/GameLogic.cpp
@@ -14,9 +14,40 @@ GameLogic::~GameLogic()
 
 void GameLogic::startGame()
 {
+	// Set up the mouse callback
+	mlx_mouse_hook(this->screen->getMLX(), &GameLogic::mouseButtonCallback, this);
+
+	// Set up the key callback for switching players
+	mlx_key_hook(this->screen->getMLX(), &GameLogic::keyPressCallback, this);
+
 	// Draw the initial board
 	this->screen->drawBoard();
 
 	// Start the main loop
 	mlx_loop(this->screen->getMLX());
+}
+
+/* Callbacks */
+void GameLogic::mouseButtonCallback(mouse_key_t button, action_t action, modifier_key_t mods, void *param)
+{
+	(void)mods;
+
+	GameLogic *game = static_cast<GameLogic *>(param);
+	if (action == MLX_PRESS && button == MLX_MOUSE_BUTTON_LEFT)
+	{
+		int xpos, ypos;
+		mlx_get_mouse_pos(game->screen->getMLX(), &xpos, &ypos);
+	}
+}
+
+void GameLogic::keyPressCallback(mlx_key_data_t keydata, void *param)
+{
+	GameLogic *game = static_cast<GameLogic *>(param);
+
+	// Pressing 'ESC' key exits the game
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+	{
+		std::cout << "Exiting..." << std::endl;
+		mlx_close_window(game->screen->getMLX());
+	}
 }
